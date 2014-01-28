7bf15c6aefb5d096c05d2e6a14b95e30948cb5ec
ahinger <andrewjhinger@gmail.com>
2014-01-28 12:19:21 -0500

    first

---
+++ Branching/Branching.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Branching
+{
+    partial class Branching
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputListBox = new System.Windows.Forms.ListBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputListBox
+            // 
+            this.outputListBox.FormattingEnabled = true;
+            this.outputListBox.Location = new System.Drawing.Point(12, 12);
+            this.outputListBox.Name = "outputListBox";
+            this.outputListBox.Size = new System.Drawing.Size(268, 225);
+            this.outputListBox.TabIndex = 0;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(99, 243);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Branching
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.outputListBox);
+            this.Name = "Branching";
+            this.Text = "Branching";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox outputListBox;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Branching/Branching.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Branching
+{
+    public partial class Branching : Form
+    {
+        public Branching()
+        {
+            InitializeComponent();
+
+            // Unconditional branching methods.
+            // return
+            doReturnNoValue();
+            doReturnWithValue();
+
+            // break
+            doBreak();
+
+            // goto
+            doGotoWithLabel();
+            doGotoWithSwitch();
+
+            // continue
+            doContinue();
+
+            // throw
+            doThrow();
+        }
+
+        private void doReturnNoValue()
+        {
+            // Get method name.
+            outputListBox.Items.Add("=> Method: " + System.Reflection.MethodInfo.GetCurrentMethod().Name);
+
+            int testVariable = 3;
+            int compareVariable = 4;
+
+            if (compareVariable > testVariable)
+            {
+                if (testVariable < 10)
+                {
+                    outputListBox.Items.Add("Return from if");
+                    return;
+                }
+                else
+                {
+                    // Do some more checking.
+                    switch (testVariable)
+                    {
+                        case 1:
+                            outputListBox.Items.Add("Return from switch case 1");
+                            return;
+                        case 2:
+                            outputListBox.Items.Add("Return from switch case 2");
+                            return;
+                        default:
+                            testVariable++;
+                            break;
+                    }
+                }
+            }
+            outputListBox.Items.Add("Normal method termination");
+        }
+
+        private bool doReturnWithValue()
+        {
+            bool result = false;
+
+            // Get method name.
+            outputListBox.Items.Add("=> Method: " + System.Reflection.MethodInfo.GetCurrentMethod().Name);
+
+            int myAge = 18;
+
+            if (myAge > 21)
+                result = true;
+            else if (myAge >= 18)
+            {
+                // Do some more checking.
+                switch (myAge)
+                {
+                    case 18:
+                        outputListBox.Items.Add("Return false from switch case 18");
+                        return false;
+                    case 19:
+                        outputListBox.Items.Add("Return true from switch case 19");
+                        return true;
+                    case 20:
+                        outputListBox.Items.Add("Return false from switch case 20");
+                        return false;
+                    default:
+                        myAge++;
+                        break;
+                }
+            }
+
+            outputListBox.Items.Add("Normal method termination, return value is " + result);
+            return result;
+        }
+
+        private void doBreak()
+        {
+            // Get method name.
+            outputListBox.Items.Add("=> Method: " + System.Reflection.MethodInfo.GetCurrentMethod().Name);
+
+            int partialCount = 0;
+
+            // while (or do) 
+            while (true)
+            {
+                if (partialCount > 5)
+                    break;
+                partialCount++;
+            }
+
+            // for (or foreach)
+            for (int i = 0; i < 10; i++)
+            {
+                if (i < 5)
+                    break;
+                partialCount++;
+            }
+
+            // switch
+            switch (partialCount)
+            {
+                case 5:
+                    partialCount *= 5;
+                    break;
+                case 10:
+                    partialCount *= 10;
+                    break;
+                default:
+                    partialCount++;
+                    break;
+            }
+
+            outputListBox.Items.Add("Partial count: " + partialCount);
+        }
+
+
+        private void doGotoWithLabel()
+        {
+            // Get method name.
+            outputListBox.Items.Add("=> Method: " + System.Reflection.MethodInfo.GetCurrentMethod().Name);
+
+            int anyNumber = 3;
+
+            if (anyNumber % 2 == 0)
+                goto Even;
+            else
+                goto Odd;
+
+        Odd:
+            outputListBox.Items.Add("The number is odd");
+            goto Finish;
+        Even:
+            outputListBox.Items.Add("The number is even");
+
+        Finish:
+            outputListBox.Items.Add("Conclusion");
+
+        }
+
+        private void doGotoWithSwitch()
+        {
+            // Get method name.
+            outputListBox.Items.Add("=> Method: " + System.Reflection.MethodInfo.GetCurrentMethod().Name);
+
+            string dayOfWeek = "Saturday";
+            bool isWeekend = true;
+
+            switch (dayOfWeek)
+            {
+                case "Saturday":
+                    isWeekend = true;
+                    break;
+                case "Sunday":
+                    goto case "Saturday";
+                case "Monday":
+                    isWeekend = false;
+                    break;
+                case "Tuesday":
+                    goto case "Monday";
+                case "Wednesday":
+                    goto case "Monday";
+                case "Thursday":
+                    goto case "Monday";
+                case "Friday":
+                    goto case "Monday";
+                default:
+                    outputListBox.Items.Add(dayOfWeek + " is not a valid week day");
+                    break;
+            }
+
+            outputListBox.Items.Add(dayOfWeek + (isWeekend ? " is " : " is not") + "a week day");
+        }
+
+        private void doContinue()
+        {
+            // Get method name.
+            outputListBox.Items.Add("=> Method: " + System.Reflection.MethodInfo.GetCurrentMethod().Name);
+
+            int startNumber = 3;
+            int endNumber = 100;
+            int count = 0;
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                // Only cound odd numbers
+                if (i % 2 == 0)
+                    continue;
+                count++;
+            }
+
+            outputListBox.Items.Add("Odd number count: " + count);
+        }
+
+        private void doThrow()
+        {
+            // Get method name.
+            outputListBox.Items.Add("=> Method: " + System.Reflection.MethodInfo.GetCurrentMethod().Name);
+
+
+            int numerator = 10;
+            int denominator = 0;
+            int result = 0;
+
+            try
+            {
+                if (denominator == 0)
+                    throw new DivideByZeroException();
+                else
+                    result = numerator / denominator;
+            }
+            catch (Exception ex)
+            {
+                outputListBox.Items.Add(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+}
